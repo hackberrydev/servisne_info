f8e4ff0e062ff0a7578887eb7cd35105a2720bb9
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2018-12-01 16:56:05 +0100

    Add streets to factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "john+#{n}@example.com" }
     password "pass123"
+    streets "Baker street"
 
     trait :admin do
       admin true
