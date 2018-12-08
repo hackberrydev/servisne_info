e015ee9db3c712aeed0214d37625e57c2da01ee6
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2018-12-08 15:36:45 +0100

    Validate presence of article title and content

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   validates :url, :presence => true
+  validates :title, :presence => true
+  validates :content, :presence => true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe Article, type: :model do
       expect(article.valid?).to be_falsy
       expect(article.errors[:url]).to be_present
     end
+    
+    it "must have title" do
+      article = FactoryBot.build(:article, :title => nil)
+      
+      expect(article.valid?).to be_falsy
+      expect(article.errors[:title]).to be_present
+    end
+    
+     it "must have content" do
+      article = FactoryBot.build(:article, :content => nil)
+      
+      expect(article.valid?).to be_falsy
+      expect(article.errors[:content]).to be_present
+    end
   end
 end
