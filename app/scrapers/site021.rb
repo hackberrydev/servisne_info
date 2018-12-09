5c4b548cf4c50d365f3e8be054f5988bbca4cf67
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2018-12-09 16:23:17 +0100

    Scrape article content and url

--- app/scrapers/site021.rb
+++ app/scrapers/site021.rb
@@ -2,11 +2,24 @@ class Site021
   def scrape
     page = mechanize.get("https://www.021.rs/Novi-Sad/Servisne-informacije/6")
     page.search(".col-md-8 .storyCatList .article_wrapper").map do |article_html|
-      article = Article.new(
-        :title => article_html.at(".article_title a span").text.strip,
-        :url => article_html.at(".article_title a").attr("href").strip
-      )
+      build_article(article_html)
     end
+  end
+  
+  private
+  
+  def build_article(article_html)
+    article = Article.new(
+      :title => article_html.at(".article_title a span").text.strip,
+      :url => article_html.at(".article_title a").attr("href").strip
+    )
+    
+    page = mechanize.get(article.url)
+    intro = page.at(".intro").text.strip
+    body = page.at(".bodyText").text.strip
+    article.content = intro + body
+    
+    article
   end
   
   def mechanize
--- spec/scrapers/site021_spec.rb
+++ spec/scrapers/site021_spec.rb
@@ -11,5 +11,7 @@ RSpec.describe Site021 do
     article = articles.third
     expect(article.title).to eq("Limani III i IV danas bez tople vode, Spens, NIS i Merkator bez grejanja")
     expect(article.url).to eq("https://www.021.rs/story/Novi-Sad/Servisne-informacije/203381/Limani-III-i-IV-danas-bez-tople-vode-Spens-NIS-i-Merkator-bez-grejanja.html")
+    expect(article.content).to match(/Limani III i IV u subotu 8. decembra biće bez tople vode./)
+    expect(article.content).to match(/Od 11 časova bez grejanja su Spens, zgrada NIS-a i Merkator./)
   end
 end
