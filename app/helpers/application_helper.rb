3c73ee73df15710b8b1041a645ec44345ce0f4fd
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2018-12-23 19:46:51 +0100

    Implement nav link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def admin_page?
     request.path.start_with?("/admin")
   end
+  
+  def nav_link(title, path)
+    content_tag :li, :class => "nav-item" do
+      link_to title, path, :class => "nav-link"
+    end
+  end
 end
