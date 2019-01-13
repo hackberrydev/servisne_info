c6819b989b27c26fc8a010ef4440d51849d1f3fc
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2019-01-13 15:40:10 +0100

    Set deploy directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "servisne_info"
+set :application, "servisne"
 set :repo_url, "git@github.com:hackberryco/servisne_info.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/developer/application_name"
+set :deploy_to, "/home/servisne/servisne"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
