7b7ad6f11773cd35a2a1389ea5f93ab735864c89
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-01-27 16:40:38 +0000

    Fix reading Sentry DSN

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   Raven.configure do |config|
-    config.dsn = Rails.application.secrets.sentry_dsn!
+    config.dsn = Rails.application.credentials.sentry_dsn
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end
