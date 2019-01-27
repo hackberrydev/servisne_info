a880fc777ddba4a60c63c3997f089145b7b31d08
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-01-27 17:03:54 +0000

    Configure SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    :address        => Rails.application.credential.smtp_settings_address,
+    :domain         => Rails.application.credential.smtp_settings_domain,
+    :user_name      => Rails.application.credential.smtp_settings_username,
+    :password       => Rails.application.credential.smtp_settings_password,
+    :port           => 587,
+    :authentication => :plain
+  }
 end
