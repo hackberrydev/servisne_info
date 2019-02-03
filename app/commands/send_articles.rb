391d0193ed2dbcb590332e22878fd680a4781347
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2019-02-03 18:05:11 +0100

    Create event when sending email

--- app/commands/send_articles.rb
+++ app/commands/send_articles.rb
@@ -12,12 +12,18 @@ class SendArticles
     User.find_each do |user|
       articles = user_articles(user)
       if articles.any?
-        puts "Sending articles to user #{user.id}"
-        UserMailer.new_articles(user, articles).deliver_now
+        send_email_to_user(user, articles)
       else
         puts "Skipping user #{user.id}"
       end
     end
+  end
+  
+  def send_email_to_user(user, articles)
+    puts "Sending articles to user #{user.id}"
+    UserMailer.new_articles(user, articles).deliver_now
+    
+    Event.create!(:message => "Sent email to user #{user.email}")
   end
   
   def user_articles(user)
