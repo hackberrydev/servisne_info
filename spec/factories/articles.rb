c8529c696e744277bdeb2e44a6a16464778bd964
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-03-20 21:07:18 +0000

    Update FactoryBot syntax

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
     sequence(:url) { |n| "example.com/article-#{n}" }
-    title "MyString"
-    content "MyText"
+    title { "MyString" }
+    content { "MyText" }
   end
 end
