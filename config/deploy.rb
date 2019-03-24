862d0ac457c4caa0d3b1acad2e451822c1bd0a3f
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-03-24 19:47:25 +0000

    Add .bundle to array of linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,8 @@ append :linked_files, "config/master.key"
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
+append :linked_dirs, ".bundle"
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
