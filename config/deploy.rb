6c90cd735220f57c7275aeefff3f5f567c2979c6
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-03-24 18:20:24 +0000

    Set Ruby version in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :repo_url, "git@github.com:hackberryco/servisne_info.git"
 set :deploy_to, "/home/servisne/servisne"
 
 set :rbenv_type, :user
-set :rbenv_ruby, File.read(".ruby-version").strip
+set :rbenv_ruby, "2.6.2"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
