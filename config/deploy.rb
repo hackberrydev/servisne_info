aa15b51d1ef8b7a9e96732e1b3aadb7d9806c61f
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-03-27 20:20:32 +0000

    Don't map bins

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,6 @@ set :deploy_to, "/home/servisne/servisne"
 set :rbenv_type, :user
 set :rbenv_ruby, "2.5.1"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
 # Default value for :format is :airbrussh.
