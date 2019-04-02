ff4d0666ddb0fee41848246191ca6d78a318fefe
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-04-02 15:43:38 +0000

    Add terms page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, 
-             :controllers => { 
+  devise_for :users,
+             :controllers => {
                :confimations => "users/confirmations",
                :passwords => "users/passwords",
                :registrations => "users/registrations",
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
              }
 
   root "pages#index"
-  
+
   get "/about", :to => "pages#about"
   get "/contact", :to => "pages#contact"
+  get "/terms", :to => "pages#terms"
 
   namespace :admin do
     resources :users, :only => :index
