25ffa677985c2f8ffffb320ac170171689a71e58
Tanja Stjepanović Stričević <stricevictanja@gmail.com>
2019-05-11 16:42:14 +0200

    Refactor users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,27 +1,31 @@
 class Admin::UsersController < Admin::ApplicationController
+  before_action :find_user, :only => [:destroy, :edit, :update]
+  
   def index
     @users = ::User.order(:created_at => :desc).page(params[:page])
   end
   
   def destroy
-    user = ::User.find(params[:id])
-    user.destroy
+    @user.destroy
     
     flash[:notice] = "User was succesfully deleted."
     redirect_to admin_users_path
   end
   
-  def edit
-    @user = ::User.find(params[:id])
-  end
+  def edit; end
   
   def update
-    @user = ::User.find(params[:id])
     if @user.update(:streets => params[:user][:streets])
       flash[:notice] = "User was succesfully saved."
       redirect_to admin_users_path
     else
       render :edit
     end
+  end
+  
+  private
+  
+  def find_user
+    @user = ::User.find(params[:id])
   end
 end
