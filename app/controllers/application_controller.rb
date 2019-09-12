078f42467245322321d89b4fdec4a17c1e6b756f
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2019-09-12 16:35:02 +0000

    Protect from forgery with exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery :prepend => true
+  protect_from_forgery :prepend => true, :with => :exception
 
   before_action :set_raven_context
 
