cb667d9a82b3793d3f4f78849cdf6a6654bda778
Nebojša Stričević <strika@hackberry.dev>
2020-05-19 19:31:13 +0000

    Update Capistrano lock version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.14.0"
 
 set :application, "servisne"
 set :repo_url, "git@github.com:hackberryco/servisne_info.git"
