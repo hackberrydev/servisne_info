52a406b8710a59af1509b9a6987b488e3ff1e02d
Nebojša Stričević <strika@hackberry.dev>
2021-03-17 19:35:30 +0000

    Revert "Update rbenv prefix"
    This reverts commit 5fdc73b23653578a4ce30dd178193a957529e92d.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to, "/home/servisne_info/servisne_info"
 
 set :rbenv_type, :user
 set :rbenv_ruby, "2.7.2"
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} rbenv exec"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_roles, :all # default value
 
 # Default value for :format is :airbrussh.
