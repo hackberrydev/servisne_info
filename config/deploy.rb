2a4f1d7bfcff903a858796a8350c303f106f876b
Nebojša Stričević <strika@hackberry.dev>
2021-03-21 14:40:43 +0000

    Deploy current branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "servisne_info"
 set :repo_url, "https://github.com/hackberrydev/servisne_info.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/servisne_info/servisne_info"
