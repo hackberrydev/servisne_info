df01f76dc69cf07c29ccb8830a793a70985b22d7
Nebojša Stričević <strika@hackberry.dev>
2022-11-08 06:32:16 +0000

    Include Model::PgSearch in Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,14 @@
 class Article < ApplicationRecord
-  include PgSearch
-  
+  include Model::PgSearch
+
   validates :title, :content, :presence => true
   validates :url, :presence => true, :uniqueness => true
-  
+
   pg_search_scope :search_by_street, :against => [:title, :content], :ignoring => :accents
-  
+
   scope :recent, -> { order(:created_at => :desc).limit(10) }
   scope :pending, -> { where(:pending => true) }
-  
+
   def self.mark_all_done
     update_all(:pending => false)
   end
