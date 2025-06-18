51c739c5f289e5dd8926297137fe278ef39c8bc5
Nebojša Stričević <strika@hackberry.dev>
2025-06-18 07:24:39 +0200

    Add some space between buttons

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -18,7 +18,7 @@ module AdminHelper
     link_to(
       "Edit",
       edit_admin_user_path(user),
-      class: "btn btn-small btn-info"
+      class: "btn btn-small btn-info mr-1"
     )
   end
 end
